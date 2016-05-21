7d226ee58306326720219ab9f26e7d5b097a5ea1
jvillarroel123 <javier.villarroel.oyarzun@gmail.com>
2016-05-21 18:09:52 -0400

    Eloquent Crear, Actualizar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,29 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\User;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/create', function () {
+    
+    $user = User::create([
+    	'name' => 'Javier Villarroel',
+    	'email' => 'javier@gmail.com',
+    	'password' => bcrypt('123123'),
+    	'gender' => 'm',
+    	'bio' => "ingeniero"
+    	]);
+
+    return "Usuario guardado";
+});
+
+Route::get('/update-user', function () {
+    
+    $user = User::find(1);
+
+    $user->gender = 'f';
+    $user->bio = "ingeniero";
+
+    $user->save();
+
+    return "Usuario actualizado";
 });
